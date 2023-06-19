025c6d133e982300241dd26da44e3bbea2171fb9
Adrian Niezgoda <adrian01234@gazeta.pl>
2023-06-19 18:38:43 +0200

    Set max and min value

--- src/js/script.js
+++ src/js/script.js
@@ -219,11 +219,19 @@ const select = {
       const thisWidget = this;
 
       const newValue = parseInt(value);
+      const minValue = settings.amountWidget.defaultMin;
+      const maxValue = settings.amountWidget.defaultMax;
 
       /* TODO: Add validation */
         
       if(thisWidget.value !== newValue && !isNaN(newValue)) {
         thisWidget.value = newValue;
+      }
+      if(thisWidget.value < minValue){
+        thisWidget.value = minValue - 1;
+      }
+      if(thisWidget.value > maxValue){
+        thisWidget.value = maxValue + 1;
       }
       thisWidget.input.value = thisWidget.value;
     }
